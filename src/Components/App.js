c86bda1837e4e120d36cc822a8d7b01d4e9ebd5f
Antkeo1 <sleav143@gmail.com>
2019-03-17 10:15:56 -0400

    created searchbar component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './SearchBar'
 
 const App = () => {
     return (
-      <div>App</div>
+      <div><SearchBar /></div>
   )
 }
 
