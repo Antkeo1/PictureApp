538726e299e8dc77e6d05a057d868d0a73027fd0
Antkeo1 <sleav143@gmail.com>
2019-03-18 10:54:31 -0400

    fixed axios GET call

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,15 +4,23 @@ import SearchBar from './SearchBar'
 
 
 class App extends React.Component {
+  state = {
+    images: []
+  }
+
   // axios unsplash api call GET
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  onSearchSubmit = async (term) => {
+    const res = await axios
+    .get('https://api.unsplash.com/search/photos', {
       params: {
         query: term
       },
       headers: {
         Authorization: 'Client-ID 042413308bf74f6c51f41513b675180f20d8c2ab7973fe90fccbe4c64c04a178'
       }
+    })
+    this.setState({
+      images: res.data.results
     })
   }
 
@@ -21,6 +29,7 @@ class App extends React.Component {
       <div className="ui container"
         style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     )
   }
