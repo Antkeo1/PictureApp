c1f0d5b84cc21607ec7ae4557a13e8fcc17111b8
Antkeo1 <sleav143@gmail.com>
2019-03-19 18:16:43 -0400

    updated title in package.json

---
+++ build/precache-manifest.edc1c31103c40a70cf2a29814f94b962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b145d5ba02722609dea",
+    "url": "/PictureApp/static/js/runtime~main.4b145d5b.js"
+  },
+  {
+    "revision": "a56e401475c521c2d094",
+    "url": "/PictureApp/static/js/main.a56e4014.chunk.js"
+  },
+  {
+    "revision": "e053a1da17cbfeebd8e3",
+    "url": "/PictureApp/static/js/2.e053a1da.chunk.js"
+  },
+  {
+    "revision": "a56e401475c521c2d094",
+    "url": "/PictureApp/static/css/main.5738c271.chunk.css"
+  },
+  {
+    "revision": "e5a796d6e4c08a3b8d7aee97dab95aaf",
+    "url": "/PictureApp/index.html"
+  }
+];
